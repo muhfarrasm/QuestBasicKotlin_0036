1cf71dadfd1831fa40c67ed1711e4919b1bf20c1
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-10 11:09:46 +0700

    Menambahkan class motor dan class contact

---
+++ app/src/main/java/com/example/myandroidapps/Class.kt
@@ -0,0 +1,19 @@
+package com.example.myandroidapps
+
+class Motor {
+    class Contact(val id: Int, var email: String)
+}
+
+// Fungsi main harus berada di luar kelas
+fun main() {
+    val contact = Motor.Contact(1, "mary@gmail.com")
+
+    // Print the value of the property: email
+    println(contact.email)
+
+    // Updates the value of the property: email
+    contact.email = "jane@gmail.com"
+
+    // Print the new value of the property: email
+    println(contact.email)
+}
---
+++ app/src/main/java/com/example/myandroidapps/TipeData.kt
@@ -0,0 +1,43 @@
+package com.example.myandroidapps
+
+fun main() {
+    // Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+
+    //Short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    // Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    // Long
+    val longValue: Long = 40
+    println("Long: $longValue")
+
+    // Float
+    val floatValue: Float = 50.2f
+    println("Float: $floatValue")
+
+    //Double
+    val doubleValue: Double = 50.2
+    println("Double: $doubleValue")
+
+    //Class
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+    //Boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    //String
+    var stringValue: String = "Hello"
+    println("String: $stringValue")
+
+    stringValue = "Hello farras"
+    println("String: $stringValue")
+}
