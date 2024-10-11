b2cdfa2cb1edf548a897a30ff22f8ea99a040a41
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-11 14:18:07 +0700

    Map Mutable adalah struktur data yang sangat berguna ketika kamu perlu menyimpan dan mengelola koleksi data dalam bentuk pasangan kunci-nilai yang dapat berubah.
    Dalam contoh ini:
    
    Kunci:
    
    "Circle" adalah kunci yang mengidentifikasi nilai 1.
    "Square" adalah kunci yang mengidentifikasi nilai 2.
    "Triangle" adalah kunci yang mengidentifikasi nilai 3.
    Nilai:
    
    1 adalah nilai yang terkait dengan kunci "Circle".
    2 adalah nilai yang terkait dengan kunci "Square".
    3 adalah nilai yang terkait dengan kunci "Triangle".

--- app/src/main/java/com/example/myandroidapps/Collections.kt
+++ app/src/main/java/com/example/myandroidapps/Collections.kt
@@ -78,4 +78,8 @@ fun ContohMao() {
     val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(readOnlyShape)
 
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println (shape)
+
 }
