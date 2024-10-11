faf90436a244264cbcf42c3244232c17ea817e0f
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-11 13:43:12 +0700

    Untuk menghapus data "Circle" dari variable shape

--- app/src/main/java/com/example/myandroidapps/Collections.kt
+++ app/src/main/java/com/example/myandroidapps/Collections.kt
@@ -54,4 +54,12 @@ fun ContohSet() {
     // Menambahkan data ke dalam Set Mutable
     shape.add("Rectangle")
     println(shape)
+
+    // Menghapus data dari Set Mutable
+    shape.remove ("Circle")
+    println(shape)
+
+
+
+
 }
