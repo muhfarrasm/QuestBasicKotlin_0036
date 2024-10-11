c7286d6fa472131b1a152adbca81fb620b5fccdf
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-11 13:28:30 +0700

    Untuk menambahkan data ke dalam Set Mutable

--- app/src/main/java/com/example/myandroidapps/Collections.kt
+++ app/src/main/java/com/example/myandroidapps/Collections.kt
@@ -51,5 +51,7 @@ fun ContohSet() {
     val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
     println(shape)
 
-
+    // Menambahkan data ke dalam Set Mutable
+    shape.add("Rectangle")
+    println(shape)
 }
