19152e0413ba85aba4d68c8a8176f9f3a6a58385
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-11 15:28:24 +0700

    Kalo yang ini pakai do while
    do-while memastikan bahwa blok kode di dalam do akan dieksekusi setidaknya sekali, terlepas dari apakah kondisi while benar atau tidak.
    
    Dalam kode ini, loop akan berhenti ketika nilai y lebih besar dari 5. Setelah iterasi ke-5, y akan menjadi 6, dan kondisi while (y <= 5) tidak lagi terpenuhi, sehingga loop berhenti.

--- app/src/main/java/com/example/myandroidapps/ConditionalExpression.kt
+++ app/src/main/java/com/example/myandroidapps/ConditionalExpression.kt
@@ -58,7 +58,11 @@ while (x <= 5){
     println("Perulangan ke-$x")
     x++
 }
-
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    }while (y <= 5)
 }
 fun main() {
     ConditionalStatment()
