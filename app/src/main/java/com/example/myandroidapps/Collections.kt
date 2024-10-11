fe0505d50bc06130b6d94bb77cc79fd4a3e8a789
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-11 14:20:00 +0700

    Menambahkan data reactangle dengan nilai 4

--- app/src/main/java/com/example/myandroidapps/Collections.kt
+++ app/src/main/java/com/example/myandroidapps/Collections.kt
@@ -82,4 +82,7 @@ fun ContohMao() {
     val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println (shape)
 
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println (shape)
 }
