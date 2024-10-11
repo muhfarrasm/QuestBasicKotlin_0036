b770e2d0a9daee8fe1a52dfff210e03434a8635f
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-11 20:07:43 +0700

    Fungsi ini untuk memberikan nilai default di salah satu parameternya, kalo di case ini adalah name. Jadi kita tidak perlu memasukkan data ke name

--- app/src/main/java/com/example/myandroidapps/Functions.kt
+++ app/src/main/java/com/example/myandroidapps/Functions.kt
@@ -18,3 +18,10 @@ fun withNamedArgument(name: String, age: Int) {
     println("=== withNamedArgument ===")
     println("Hello, $name! You are $age years old")
 }
+
+// default parameter value
+// default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Farras", age: Int){
+    println("=== withDefaultParameter ===")
+    println("Hello, $name! You are $age years old")
+}
