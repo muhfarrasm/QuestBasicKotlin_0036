9a4e7aeeada3ab93c0345c96f0a0476d72b1842d
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-11 14:39:07 +0700

    Membuat function Conditional Expression

---
+++ app/src/main/java/com/example/myandroidapps/ConditionalExpression.kt
@@ -0,0 +1,11 @@
+package com.example.myandroidapps
+
+// Conditional Expression
+// Conditional Expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional Expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatment() {
+    println("=== Conditional Expression ===")
+
+
+}
