585af27057f20196358a41b6710c475fbad79ab6
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-11 19:31:03 +0700

    membuat data class bernama "DataClass". Untuk menyimpan data tetap yaitu id, dan menyimpan data yang bisa diubah yaitu email

---
+++ app/src/main/java/com/example/myandroidapps/DataClass.kt
@@ -0,0 +1,11 @@
+package com.example.myandroidapps
+
+//Data class
+// Data class adalah class yang digunakan untuk menyimpan data.
+// Data class menyediakan fungsi untuk meng-overide fungsi equals(), hashCode(), dan toString().
+// Data class juga menyediakan fungsi copy() untuk membuat salinan objek dengan dengan beberapa perubahan
+
+data class DataClass(
+    val id: Int,
+    var email: String
+)
