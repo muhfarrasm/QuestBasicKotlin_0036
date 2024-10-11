05a5b6c1979c81fdaf4d3a8626e31dfefe89caa4
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-11 20:03:26 +0700

    Fungsi withNamedArgument dalam Kotlin yang kamu buat menerima dua parameter: name (tipe String) dan age (tipe Int).

--- app/src/main/java/com/example/myandroidapps/Functions.kt
+++ app/src/main/java/com/example/myandroidapps/Functions.kt
@@ -11,3 +11,10 @@ fun withParameter(name: String){
     println("Hello, $name!")
 }
 
+// named argument
+// named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("=== withNamedArgument ===")
+    println("Hello, $name! You are $age years old")
+}
